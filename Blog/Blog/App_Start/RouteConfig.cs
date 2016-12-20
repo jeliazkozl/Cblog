44d8ea7519195aba788bf6b9eef8efc422d4a0d6
jeliazkozl <Le0n@mail.bg>
2016-12-20 11:23:49 +0200

    Cblog
    Cblog

---
+++ Blog/Blog/App_Start/RouteConfig.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Blog
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+
+            // Deny access to axd files
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            // Default routes in the WebApp
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ Blog/Blog/Models/ArticleViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Blog.Models
+{
+    public class ArticleViewModel
+    {
+        public int Id { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public List<Category> Categories { get; set; }
+
+        public string Tags { get; set; }
+
+        public ICollection<Article> Articles { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Title { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+
+        public string AuthorId { get; set; }
+        
+    }
+}
